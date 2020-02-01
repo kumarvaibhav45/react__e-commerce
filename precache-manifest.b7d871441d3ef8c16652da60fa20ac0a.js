20dc36f6c92478d1fd6bb441b8e9ced9d32a8bf3
kumarvaibhav45 <wave.vaibhav@gmail.com>
2020-02-01 14:03:04 +0530

    Updates

---
+++ precache-manifest.b7d871441d3ef8c16652da60fa20ac0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc9d6e3b2e195d702e9d7c451aebb07",
+    "url": "/react_e-commerce/index.html"
+  },
+  {
+    "revision": "323add075fe33f686352",
+    "url": "/react_e-commerce/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "a634036fe9e0cffea01a",
+    "url": "/react_e-commerce/static/css/main.cc7308de.chunk.css"
+  },
+  {
+    "revision": "323add075fe33f686352",
+    "url": "/react_e-commerce/static/js/2.ccc62231.chunk.js"
+  },
+  {
+    "revision": "a634036fe9e0cffea01a",
+    "url": "/react_e-commerce/static/js/main.a46493c9.chunk.js"
+  },
+  {
+    "revision": "3b4e1c3f24bad75e9b1a",
+    "url": "/react_e-commerce/static/js/runtime-main.29e300d1.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/react_e-commerce/static/media/logo.9f7e506d.svg"
+  }
+]);
